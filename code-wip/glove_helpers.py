3720c2924f70c01d3c78a027c1295ce3872b6018
Jacob Greenfield <jag625@lehigh.edu>
2023-04-18 23:03:58 -0400

    Create fully-integrated Jupyter notebook

--- code-wip/glove_helpers.py
+++
@@ -1,9 +0,0 @@
-import struct
-from scipy.sparse import coo_matrix
-
-def load_cooccurrences(path, *args, **kwargs):
-  """ Usage: load_cooccurrences("cooccurrence.bin") """
-  with open(path, "rb") as f:
-    raw = f.read()
-  i, j, data = zip(*struct.iter_unpack("@iid", raw))
-  return coo_matrix((data, (i, j)), *args, **kwargs)
